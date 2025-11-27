f87770b4a1a68822b4106ed24722b1fb5e31df94
Nathan <Nathan.bergman@mail.utoronto.ca>
2025-11-27 13:01:05 -0500

    Renamed main files

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,18 +1,90 @@
 package app;
 
+import data_access.FileUserDataAccessObject;
 import data_access.SportsAPIDataAccess;
-import entity.User;
+import entity.UserFactory;
+import interface_adapter.ViewManagerModel;
+
+import interface_adapter.login.LoginController;
+import interface_adapter.login.LoginPresenter;
+import interface_adapter.login.LoginViewModel;
+
+import interface_adapter.signup.SignupController;
+import interface_adapter.signup.SignupPresenter;
+import interface_adapter.signup.SignupViewModel;
+
+import interface_adapter.loggedin.LoggedInViewModel;
+
+import use_case.login.LoginInteractor;
+import use_case.signup.SignupInteractor;
+
 import view.LoginView;
-import view.MainMenuFrame.MainMenuFrame;
+import view.SignupView;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
-    public static void main(String[] args) {
-        SportsAPIDataAccess data = new SportsAPIDataAccess();
 
-        // data.fetchOdds();
+    public static void main(String[] args) {
+
+        System.setProperty("sun.java2d.uiScale", "1");
+
+        SportsAPIDataAccess data = new SportsAPIDataAccess();
         data.readdata();
-        User user = new User("BillChen", 1525.00, 15, 8,"123456");
-        new MainMenuFrame(user);
-        //test_push
+
+        UserFactory userFactory = new UserFactory();
+        final FileUserDataAccessObject userDataAccessObject = new FileUserDataAccessObject("users.csv", userFactory);
+
+
+        ViewManagerModel viewManager = new ViewManagerModel();
+        LoginViewModel loginVM = new LoginViewModel();
+        SignupViewModel signupVM = new SignupViewModel();
+        LoggedInViewModel loggedInVM = new LoggedInViewModel();
+
+        LoginPresenter loginPresenter =
+                new LoginPresenter(viewManager, loggedInVM, loginVM, signupVM, userDataAccessObject);
+
+        SignupPresenter signupPresenter =
+                new SignupPresenter(viewManager, signupVM, loginVM);
+
+        LoginInteractor loginInteractor =
+                new LoginInteractor(userDataAccessObject, loginPresenter);
+
+        SignupInteractor signupInteractor =
+                new SignupInteractor(userDataAccessObject, signupPresenter, userFactory);
+
+        LoginController loginController =
+                new LoginController(loginInteractor);
+
+        SignupController signupController =
+                new SignupController(signupInteractor);
+
+        LoginView loginView = new LoginView(loginVM);
+        loginView.setLoginController(loginController);
+
+        SignupView signupView = new SignupView(signupVM);
+        signupView.setSignupController(signupController);
+
+        JFrame frame = new JFrame("App Test");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(450, 350);
+
+        JPanel cards = new JPanel(new CardLayout());
+        cards.add(loginView, loginView.getViewName());
+        cards.add(signupView, signupView.getViewName());
+
+        frame.setContentPane(cards);
+        frame.setVisible(true);
+
+        viewManager.addPropertyChangeListener(evt -> {
+            String viewName = (String) evt.getNewValue();
+            CardLayout layout = (CardLayout) cards.getLayout();
+            layout.show(cards, viewName);
+            System.out.println("Switched to view: " + viewName);
+        });
+
+        viewManager.setState(loginView.getViewName());
+        viewManager.firePropertyChange();
     }
 }
